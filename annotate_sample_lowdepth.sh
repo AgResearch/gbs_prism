35d4cf6fd6a1fff9099e2e49807a71e86714843e
afmcc <alanfmcculloch@gmail.com>
2018-10-15 14:38:05 +1300

    best rather than top hit for spot-checks

--- annotate_sample_lowdepth.sh
+++ annotate_sample_lowdepth.sh
@@ -22,27 +22,16 @@ fi
 
 
 sample_fasta=$1
-# example : /dataset/gseq_processing/scratch/gbs/180921_D00390_0400_BCCVDJANXX/SQ0799.all.DEER.PstI/fasta_medium_lowdepthsample/qc304501-1_CCVDJANXX_2_799_X4.cnt.tag_count_unique.s.05m2T10_taggt2.fasta
 output_folder=$2
-#example : /dataset/gseq_processing/scratch/gbs/180921_D00390_0400_BCCVDJANXX/SQ0799.all.DEER.PstI/annotation
 
 cd $output_folder/..   # attempt to use relevant run time e.g. tardis.toml
 
+#time $SEQ_PRISMS_BIN/align_prism.sh -f -a blastn  -r nt  -p "-evalue 1.0e-10  -dust \'20 64 1\'  -outfmt \'7 qseqid sseqid pident evalue staxids sscinames scomnames sskingdoms stitle\'"  -O $output_folder $sample_fasta 
 
-time $SEQ_PRISMS_BIN/align_prism.sh -f -a blastn  -r nt  -p "-evalue 1.0e-10  -dust \'20 64 1\' -max_target_seqs 1 -outfmt \'7 qseqid sseqid pident evalue staxids sscinames scomnames sskingdoms stitle\'"  -O $output_folder $sample_fasta 
+base=`basename $sample_fasta .fasta`
+moniker=`echo $base | awk -F_ '{print $1}' -`
+$SEQ_PRISMS_BIN/taxonomy_prism.py  --column_numbers 0,3,7,6  --summary_type dump_top_hits --top_hit_selection_method best $output_folder/$moniker*.results.gz > $output_folder/${moniker}.best_hits.txt 
 
-base=`basename $sample_fasta`
-qcid=`echo $base | awk -F- '{print $1}' -`
 
-gunzip $output_folder/${qcid}*.results.gz
-echo "All hits:"
-grep "hits found" $output_folder/${qcid}*.results  | grep -v " 0 hits found" | wc
-
-echo "Bacterial hits:"
-grep -A 1 "hits found"  $output_folder/${qcid}*.results | egrep "^seq" | grep Bacteria | wc
-
-echo "Non-bacterial hits:"
-grep -A 1 "hits found"  $output_folder/${qcid}*.results | egrep "^seq" | grep -v Bacteria | wc
-
-set +x
-
+# example further summary:
+#cat /dataset/gseq_processing/scratch/gbs/181005_D00390_0407_BCCV91ANXX/SQ0807.all.PstI.PstI/annotation/qc314325-1.best_hits.txt | grep -v "No hits" | awk -F, '{print $4}' -  | sort | uniq -c | sort -n -k 1,1
