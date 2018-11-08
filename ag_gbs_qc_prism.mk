05d830150f56fbf27d606aba276a2d874b6e955b
afmcc <alanfmcculloch@gmail.com>
2018-11-08 14:11:10 +1300

    fix clean

--- ag_gbs_qc_prism.mk
+++ ag_gbs_qc_prism.mk
@@ -47,19 +47,12 @@
 	$@.sh
 	date > $@
 
+.PHONY: %.clean
 %.clean: 
 	$@.sh
-	date > $@
 
 
 ##############################################
 # specify the intermediate files to keep 
 ##############################################
 .PRECIOUS: %.log %.ag_gbs_qc_prism %.blast_analysis %.kmer_analysis %.kgd %.demultiplex %.all %.fasta_sample
-
-##############################################
-# cleaning - not yet doing this using make  
-##############################################
-clean:
-	echo "no clean for now" 
-
