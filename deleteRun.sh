1041f881afbab1305fd6eb1425df9bd631342cbb
afmcc <alanfmcculloch@gmail.com>
2019-05-08 14:49:36 +1200

    added a script to delete a run from the database (not including deleting keyfiles)

---
+++ deleteRun.sh
@@ -0,0 +1,159 @@
+#!/bin/sh
+#
+# this scripts sets up a run in the database   
+# to use it - e.g.
+# deleteRun.sh 150630_D00390_0232_AC6K0WANXX 
+# jumps through various hoops due to idiosyncracies of 
+# \copy and copy
+# only intended for importing GBS runs at this stage
+#
+function get_opts() {
+
+help_text="\n
+      usage : deleteRun.sh -r run_name\n
+      example (dry run) : ./deleteRun.sh -n -r 190506_D00390_0455_ACDNN7ANXX\n
+      example           : ./deleteRun.sh -r 190506_D00390_0455_ACDNN7ANXX\n
+"
+
+DRY_RUN=no
+RUN_NAME=""
+MACHINE=hiseq
+RUN_BASE_PATH=/dataset/gseq_processing/scratch/gbs
+
+while getopts ":nhr:m:d:" opt; do
+  case $opt in
+    n)
+      DRY_RUN=yes
+      ;;
+    i)
+      INTERACTIVE=yes
+      ;;
+    r)
+      RUN_NAME=$OPTARG
+      ;;
+    m)
+      MACHINE=$OPTARG
+      ;;
+    h)
+      echo -e $help_text
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+KEY_DIR=/dataset/hiseq/active/key-files
+}
+
+function check_opts() {
+if [ -z "$GBS_PRISM_BIN" ]; then
+   echo "GBS_PRISM_BIN not set - quitting"
+   exit 1
+fi
+
+if [ -z "$RUN_NAME" ]; then
+   echo -e $help_text
+   exit 1
+fi
+
+if [ $MACHINE == "miseq" ]; then
+   echo "error , database import of miseq runs not currently supported"
+   exit 1
+fi
+
+in_db=`$GBS_PRISM_BIN/is_run_in_database.sh $RUN_NAME`
+if [ $in_db != "1" ]; then
+   echo "$RUN_NAME is not in database - quitting"
+   exit 1 
+fi
+
+# machine must be miseq or hiseq
+if [[ ( $MACHINE != "hiseq" ) && ( $MACHINE != "miseq" ) ]]; then
+    echo "machine must be miseq or hiseq"
+    exit 1
+fi
+
+}
+
+function echo_opts() {
+    echo "deleteing $RUN_NAME"
+    echo "DRY_RUN=$DRY_RUN"
+}
+
+get_opts $@
+
+check_opts
+
+echo_opts
+
+## from here , process the import
+
+rm -f /tmp/${RUN_NAME}.txt
+if [ -f /tmp/${RUN_NAME}.txt ]; then
+   echo "rm -f /tmp/${RUN_NAME}.txt failed - quitting"
+   exit 1
+fi
+rm -f /tmp/${RUN_NAME}.psql
+if [ -f /tmp/${RUN_NAME}.psql ]; then
+   echo "rm -f /tmp/${RUN_NAME}.psql failed - quitting"
+   exit 1
+fi
+
+
+echo "
+
+--we need to delete biosampleob records, where the biosampleob is linked to this run, and is not linked to any others
+--save these as need to delete links first ! 
+
+create table delete_run_tmp as (
+select obid from biosampleob where obid in (
+select distinct biosampleob from biosamplelistmembershiplink
+where biosamplelist = ( select
+obid from biosamplelist where listname = :run_name)
+except
+select distinct biosampleob from biosamplelistmembershiplink
+where biosamplelist != ( select
+obid from biosamplelist where listname = :run_name)
+));
+
+
+-- delete the links
+delete from biosamplelistmembershiplink 
+where biosamplelist = (select
+obid from biosamplelist where listname = :run_name );
+
+-- delete the sampleobs
+delete from biosampleob where obid in (select obid from delete_run_tmp);
+
+-- clean and drop that tmp table
+delete from delete_run_tmp;
+drop table delete_run_tmp;
+
+-- delete from hiseqsamplesheetfact
+delete from hiseqsamplesheetfact where biosamplelist = (select
+obid from biosamplelist where listname = :run_name );
+
+--delete from biosamplelist
+delete from biosamplelist where listname = :run_name;
+" > /tmp/delete_${RUN_NAME}.psql
+
+if [ $DRY_RUN == "no" ]; then
+   psql -U agrbrdf -d agrbrdf -h invincible -v run_name=\'${RUN_NAME}\' -f /tmp/delete_${RUN_NAME}.psql
+else
+   echo " will run 
+   psql -U agrbrdf -d agrbrdf -h invincible -v run_name=\'${RUN_NAME}\' -f /tmp/delete_${RUN_NAME}.psql"
+fi
+
+
+echo "
+finished deleting ${RUN_NAME}
+
+NOTE : the keyfile table (gbskeyfilefact) may still contain records relating to this flowcell
+"
