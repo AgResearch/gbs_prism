17d8cd268244c9c7ede62c705d24ee437b1afaf1
afmcc <alanfmcculloch@gmail.com>
2019-12-20 12:14:32 +1300

    not appropriate to run HWdgm.05 filtering on pooled samples

--- run_kgd.R
+++ run_kgd.R
@@ -12,16 +12,13 @@ source(file.path(Sys.getenv("SEQ_PRISMS_BIN"),"/../KGD/GBS-Chip-Gmatrix.R"))
 if ( geno_method == "default" ) {
    Gfull <- calcG()
    GHWdgm.05 <- calcG(which(HWdis > -0.05),"HWdgm.05", npc=4)  # recalculate using Hardy-Weinberg disequilibrium cut-off at -0.05
+   writeG(GHWdgm.05, "GHW05", outtype=c(1, 2, 3, 4, 5, 6))
+   writeVCF(outname="GHW05", ep=.001)
 } else if ( geno_method == "pooled" ) {
-   Gfull <- calcG(samptype=geno_method)
-   GHWdgm.05 <- calcG(which(HWdis > -0.05),"HWdgm.05", npc=4, samptype=geno_method)  # recalculate using Hardy-Weinberg disequilibrium cut-off at -0.05
+   Gfull <- calcG(samptype=geno_method, npc=4)
+   writeG(Gfull, "GFULL", outtype=c(1, 2, 3, 4, 5, 6))
+   writeVCF(outname="GFULL", ep=.001)
+   print("(not running HWdgm.05 filtering on pooled data)")
 } else {
    stop(paste("Error: geno_method ", geno_method, " is not supported"))
 } 
-
-
-#To save a G Matrix
-writeG(GHWdgm.05, "GHW05", outtype=c(1, 2, 3, 4, 5, 6))
-
-#To write out vcf file
-writeVCF(outname="GHW05", ep=.001)
