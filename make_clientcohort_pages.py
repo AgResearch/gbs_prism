b7603bfed5286ce0ab620fc53ee3a890ea0cbd39
afmcc <alanfmcculloch@gmail.com>
2022-05-17 14:35:00 +1200

    fixed the basedir output path (now can pass is as arg); added plot narratives from make_cohort_pages.py

--- make_clientcohort_pages.py
+++ make_clientcohort_pages.py
@@ -35,15 +35,13 @@ footer1="""
 </html>
 """
 
-BASEDIR="/dataset/gseq_processing/scratch/gbs"
-
-
 def get_cohorts(options):
+    BASEDIR=options["basedir"]    
     # cohorts are idenitified as subfolders of the run folder that
     # * are not tardis working folders (i.e. have names starting with tardis
     # * are of like SQ0775.all.TILAPIA.PstI-MspI
     #   - i.e. library.qc-cohort.gbs-cohort.enzyme
-    run_folder=os.path.join(BASEDIR, options["run_name"])
+    run_folder=os.path.join(BASEDIR, options["run_folder"])
     #print "DEBUG : "+run_folder
     #SQ0810.all.PstI-MspI.PstI-MspI
     #SQ0812.all.ApeKI.ApeKI
@@ -59,6 +57,7 @@ def get_cohorts(options):
     
     
 def generate_run_plot(options):
+    BASEDIR=options["basedir"]   # e.g. /bifo/scratch/hiseq/postprocessing/gbs    
     stats = {
         "found file count" : 0,
         "no file count" : 0,
@@ -74,6 +73,113 @@ def generate_run_plot(options):
     file_iters["KGD (plots)"] = [ os.path.join(options["kgd_subfolder_name"], item) for item in file_iters["KGD (plots)"] ]
     file_iters["KGD (text file links)"] = [ os.path.join(options["kgd_subfolder_name"], item) for item in file_iters["KGD (text file links)"] ]
     file_iters["Hapmap files"] = [ os.path.join(options["hapmap_subfolder_name"], item) for item in file_iters["Hapmap files"] ]
+
+    narratives = {
+        'KGD/InbCompare.png' : """
+Check clumpify normalisation (deduplication) by comparing four estimates of inbreeding: a) production estimate
+ b) adjust using fitted beta-binomial (bb) c) sampling one read per lane to remove any duplication effect (used to fit bb)
+ d) treat two lanes as two individuals. If the points are below the line in the first column (a .v. b,c,d), or alpha
+ is low, our normalisation may not be aggressive enough, as production inbreeding estimate is thus higher than these other
+ three methods (each of which should provide a robust estimate of inbreeding, even with un-normalised data)
+""",
+        'KGD/AlleleFreq.png' : """
+Comparisons SNP reference allele frequencies calculated 3 different ways: 1) after naively converting to genotypes, 2) based on allele counts (without any adjustment for multiple counts of the same allele), 3) as given by UNEAK (the same as method 2).
+""",
+        'KGD/CallRate.png' : """
+Histogram of call rate (proportion of SNPs scored) for each sample.
+""",
+        'KGD/Co-call-HWdgm.05.png' : """
+Shows the co-call distribution (see Co-call-.png) after applying the Hardy-Weinberg filter to SNPs and combining lanes (if relevant).
+""",
+        'KGD/Co-call-.png' : """
+The co-call plot shows the distribution (over all possible pairs) of the proportion of SNPs called in a pair of individuals.
+Bimodality may indicate that the samples belong to 2+ genetically divergent clusters (low co-call rate for pairs from different clusters)
+""",
+        'KGD/finplot.png' : """
+A finplot shows raw Hardy-Weinberg disequilibrium (PAA - pA^2) against MAF for each SNP. 
+SNPs near the upper border tend to be mainly homozygous. This is often because they have low depth (perhaps only one allele observed).
+SNPs near the lower border tend to be mainly heterozygous and may often represent duplicated regions erroneously treated as one region. 
+The Hardy-Weinberg filter, HWdgm.05 (Hardy-Weinberg disequilibium > -0.05) used in some analyses removes these heterozygous SNPs.
+""",
+        'KGD/Gcompare.png' : """
+Comparisons of different methods for calculating off-diagonals of the GRM. G5=KGD method, G3=KGD with single allele per genotype, G1=unadjusted (biased towards 0).
+""",
+        'KGD/GcompareHWdgm.05.png' : """
+Same as Gcompare.png plot but using only those SNPs passing the Hardy-Weinberg filter and combining lanes (if relevant)
+""",
+        'KGD/SampDepthHist.png' : """
+Distribution of sample depths. If it is not roughly unimodal, there may be contamination or a mix of different species or breeds. Also, some downstream
+ pipelines use a sample-depth cutoff to filter the data, which could be confused by a multi-modal sample-depth distribution.
+""",
+        'KGD/Gdiagdepth.png' : """
+In theory self-relatedness estimated from GBS data should be uncorrelated with sample depth, so there should be neither negative nor positive trend
+ in this plot. A negatively sloping trend has been nick-named a slippery slope, and means something is not quite right somewhere (with either the make-up of the original sample
+ -e.g. some kind of contamination, or the enzyme digest, or size selection, or sequencing, or downstream processing). If they persist, slippery slopes need to be investigated and
+ resolved
+""",
+        'KGD/GHWdgm.05diagdepth.png' : """
+This is similar to the Gdiagdepth.png plot, but 1) combining lanes (if relevant) and 2) based on a smaller set of SNPs (net of the Hardy-Weinberg filter), which eliminates some potential sources of
+ correlation between self-relatedness estimates and sample depth. So this plot should exhibit less than (or the same) correlation as that plot. 
+""",
+        'KGD/Heatmap-G5HWdgm.05.png' : """
+Heatmap representation of the G5 GRM.
+Relatedness values are coloured from white to red (lowest to highest relatedness). Samples are ordered by a dendrogram from hierarchically clustering a distance matrix of the GRM.
+SNPs have been Hardy-Weinberg filterd and and lanes combined (if relevant)
+""",
+        'KGD/HWdisMAFsig.png' : """
+SNPs plotted as in finplot.png but coloured by an approximate depth-adjusted significance value.
+""",
+        'KGD/MAF.png' : """
+MAF distribution. Diverse populations tend to have a peak at low values of MAF.
+""",
+        'KGD/MAFHWdgm.05.png' : """
+MAF distribution of Hardy-Weinberg filtered SNPs after combining lanes (if relevant)
+""",
+        'KGD/PC1v2G5HWdgm.05.png' : """
+Principal components plot (1st 2 coordinates) of samples, based on the G5 GRM.
+SNPs have been Hardy-Weinberg filterd and and lanes combined (if relevant)
+""",
+        'KGD/SampDepthCR.png' : """
+Plot of depth vs call rate for samples. This is usually a fairly tight upwardly curving line.
+""",
+        'KGD/SampDepthHist.png' : """
+Histogram of sample depths. This is usually unimodal. 
+""",
+        'KGD/SampDepth.png' : """
+Plot of mean vs median sample depth.
+""",
+        'KGD/SampDepth-scored.png' : """
+Plot of mean sample depth for SNPs with at least one read for the sample against the mean sample depth including all SNPs.
+""",
+        'KGD/SNPCallRate.png' : """
+Histogram of the proportion of samples called for a SNP. There is usually a peak at a high value and tails off to the left.
+""",
+        'KGD/SNPDepthHist.png' : """
+Histogram of mean SNP depth. There are often a few SNPs with extremely high depth, while most SNPs have relatively low depth. 
+The SNPdepth.png plot has SNP depth log-transformed to show the distribution more clearly.
+""",
+        'KGD/SNPDepth.png' : """
+Plot of SNP call rate against (log-transformed) SNP depth. Usually a S-shaped band. 
+High depth low call rate SNPs may indicate diverse populations (some SNPs only seen in a subset) (and/or possibly size selection variation?)
+""",
+        'KGD/X2star-QQ.png' : """
+Quantile-quantile plot of approximate depth-adjusted Hardy-Weinberg test statistic for each SNP. A 1-1 line indicates the theoretical distribution holds. 
+A higher sloped straight line suggests some population structure. SNPs that plot higher than a straight line through the majority of SNPs may indicate SNPs that do not behave in a Mendelian fashion.
+""",
+        'KGD/PlateDepth.png' : """
+Plot of mean sample depth by plate position. Patterns of depth variation may represent problems with sample handling.
+""",
+        'KGD/PlateInb.png' : """
+Plot of estimated inbreeding by plate position. Patterns of inbreeding variation may represent problems with sample handling.
+""",
+        'KGD/SubplateDepth.png' : """
+The same as PlateDepth.png but with a different colour gradient for each subplate within the main plate.
+""",
+        'KGD/SubplateInb.png' : """
+The same as PlateInb.png but with a different colour gradient for each subplate within the main plate.
+"""
+    } 
+    
     
     
     
@@ -91,8 +197,8 @@ def generate_run_plot(options):
     for cohort in cohorts:
 
         if options["cohort_folder" ] is None:
-            out_stream_filename = os.path.join(BASEDIR, options["run_name"], "html", cohort, options["output_filename"])
-            out_manifest_filename = os.path.join(BASEDIR, options["run_name"], "html", cohort, "%s.manifest"%options["output_filename"])
+            out_stream_filename = os.path.join(BASEDIR, options["run_folder"], "html", cohort, options["output_filename"])
+            out_manifest_filename = os.path.join(BASEDIR, options["run_folder"], "html", cohort, "%s.manifest"%options["output_filename"])
 
         print cohort, out_stream_filename, out_manifest_filename
 
@@ -123,8 +229,8 @@ def generate_run_plot(options):
 
 
                         print >> out_stream , "<tr><td>%s</td>\n"%file_name
-                        if options["run_name"] is not None:
-                            file_path = os.path.join(BASEDIR, options["run_name"], cohort, file_name)
+                        if options["run_folder"] is not None:
+                            file_path = os.path.join(BASEDIR, options["run_folder"], cohort, file_name)
                         else:
                             file_path = os.path.join(cohort, file_name)
                             
@@ -135,7 +241,11 @@ def generate_run_plot(options):
                             print >> out_manifest, image_relpath
 
                             if os.path.isfile(file_path):
-                                print >> out_stream, "<td> <img src=%s title=%s height=300 width=300/> </td>\n"%(image_relpath, file_path)
+                                title = image_relpath
+                                if file_name in narratives:
+                                    title = "\"%s (%s)\""%(image_relpath, narratives[file_name].replace('\n',''))
+                                print >> out_stream, "<td> <img src=%s title=%s height=300 width=300/> </td>\n"%(image_relpath, title)
+                               
                             else:
                                 print >> out_stream, "<td> unavailable </td>\n"
                         elif file_type == "link":
@@ -182,19 +292,21 @@ python ../gbs_prism/make_clientcohort_pages.py -U discarded_hapMap -K discarded_
     """
     parser = argparse.ArgumentParser(description=description, epilog=long_description, formatter_class = argparse.RawDescriptionHelpFormatter)
     parser.add_argument('cohort_folder', nargs='?', type=str, default=None)
-    parser.add_argument('-r', '--run_name' , dest='run_name', default = None, type=str, help="run name")
+    parser.add_argument('-r', '--run_folder' , dest='run_folder', default = None, type=str, help="run name")
     parser.add_argument('-t', '--title' , dest='title', default = None, type=str, help="title")    
     parser.add_argument('-H', '--image_height' , dest='image_height', default=300, type=int, help="image height")
     parser.add_argument('-W', '--image_width' , dest='image_width', default=300, type=int, help="image width")
     parser.add_argument('-o', '--output_filename' , dest='output_filename', default="peacock.html", type=str, help="name of output file")
     parser.add_argument('-U', '--hapmap_subfolder_name' , dest='hapmap_subfolder_name', default="hapMap", type=str, help="name of hapmap subfolder")
     parser.add_argument('-K', '--kgd_subfolder_name' , dest='kgd_subfolder_name', default="KGD", type=str, help="name of KGD sub-folder name")
+    parser.add_argument('-b', '--basedir' , dest='basedir', default="/dataset/hiseq/scratch/postprocessing/gbs", type=str, help="base dir of original output")
+    
     
     
     args = vars(parser.parse_args())
 
-    if args["run_name"] is None and args["cohort_folder"] is None:
-        raise Exception("must specify either run name or a cohort folder")
+    if args["run_folder"] is None and args["cohort_folder"] is None:
+        raise Exception("must specify either run folder or a cohort folder")
 
     return args
 
