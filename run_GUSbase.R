ce1999d2e9ee03ab83398d78db3537342850f614
afmcc <alanfmcculloch@gmail.com>
2022-06-09 11:54:05 +1200

    add reference to GUSbase package

--- run_GUSbase.R
+++ run_GUSbase.R
@@ -15,3 +15,4 @@ GUSbase::cometPlot(ref, alt, maxdepth=500, maxSNPs=1e6)
 
 #	maxdepth: Controls the maximum depth displayed on the axis. This can be quite good if there are only a few really large read depths, then it can save a lot of computational time.  
 #	maxSNPs: Maximum number of SNPs used to generate the comet plot. If there are more SNPs than this number, then a random selection will be taken. This can be quite good for reducing the computational time.
+# ref https://rdrr.io/github/tpbilton/GUSbase/man/DiagPlots.html
