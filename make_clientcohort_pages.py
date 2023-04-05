f28b41afcd2bad9ceeafe653c035625aae632c4e
afmcc <alanfmcculloch@gmail.com>
2023-04-06 09:55:18 +1200

    update client html mashup with latest plots and fix bug in attaching narratives

--- make_clientcohort_pages.py
+++ make_clientcohort_pages.py
@@ -66,7 +66,12 @@ def generate_run_plot(options):
 
     file_group_iter = (  ("KGD (plots)", "image"), ("KGD (text file links)", "link"), ("Hapmap files", "link" ) )
     file_iters = {
-        "KGD (plots)" : ['AlleleFreq.png', 'CallRate.png', 'Co-call-HWdgm.05.png', 'Co-call-.png', 'finplot.png', 'GcompareHWdgm.05.png', 'Gcompare.png', 'Gdiagdepth.png', 'GHWdgm.05diagdepth.png', 'InbCompare.png', 'Heatmap-G5HWdgm.05.png', 'HWdisMAFsig.png', 'MAFHWdgm.05.png', 'MAF.png', 'PC1v2G5HWdgm.05.png', 'SampDepthCR.png', 'SampDepthHist.png', 'SampDepth.png', 'SampDepth-scored.png', 'SNPCallRate.png', 'SNPDepthHist.png', 'SNPDepth.png', 'X2star-QQ.png'],
+        "KGD (plots)" : ['AlleleFreq.png', 'CallRate.png', 'Co-call-HWdgm.05.png', 'Co-call-.png', 'finplot.png', \
+                         'GcompareHWdgm.05.png', 'Gcompare.png', 'Gdiagdepth.png', 'GHWdgm.05diagdepth.png', \
+                         'InbCompare.png','Heatmap-G5HWdgm.05.png', 'HWdisMAFsig.png', \
+                         'MAFHWdgm.05.png', 'MAF.png', 'PC1v2G5HWdgm.05.png', 'SampDepthCR.png', 'SampDepthHist.png', \
+                         'SampDepth.png', 'SampDepth-scored.png', 'SNPCallRate.png', 'SNPDepthHist.png', 'SNPDepth.png', \
+                         'X2star-QQ.png', 'PlateDepth.png', 'PlateInb.png', 'SubplateDepth.png', 'SubplateInb.png'],
         "KGD (text file links)" : ['GHW05.csv', 'GHW05-Inbreeding.csv', 'GHW05-long.csv', 'GHW05-pca_metadata.tsv', 'GHW05-pca_vectors.tsv', 'GHW05-PC.csv', 'GHW05.RData', 'GHW05.vcf', 'HeatmapOrderHWdgm.05.csv', 'HeatmapOrderHWdgm.05.csv.blinded', 'PCG5HWdgm.05.pdf', 'SampleStats.csv', 'SampleStats.csv.blinded', 'seqID.csv', 'seqID.csv.blinded'],
         "Hapmap files" : ['HapMap.hmc.txt','HapMap.fas.txt']
     }
@@ -75,107 +80,107 @@ def generate_run_plot(options):
     file_iters["Hapmap files"] = [ os.path.join(options["hapmap_subfolder_name"], item) for item in file_iters["Hapmap files"] ]
 
     narratives = {
-        'KGD/InbCompare.png' : """
+        'InbCompare.png' : """
 Check clumpify normalisation (deduplication) by comparing four estimates of inbreeding: a) production estimate
  b) adjust using fitted beta-binomial (bb) c) sampling one read per lane to remove any duplication effect (used to fit bb)
  d) treat two lanes as two individuals. If the points are below the line in the first column (a .v. b,c,d), or alpha
  is low, our normalisation may not be aggressive enough, as production inbreeding estimate is thus higher than these other
  three methods (each of which should provide a robust estimate of inbreeding, even with un-normalised data)
 """,
-        'KGD/AlleleFreq.png' : """
+        'AlleleFreq.png' : """
 Comparisons SNP reference allele frequencies calculated 3 different ways: 1) after naively converting to genotypes, 2) based on allele counts (without any adjustment for multiple counts of the same allele), 3) as given by UNEAK (the same as method 2).
 """,
-        'KGD/CallRate.png' : """
+        'CallRate.png' : """
 Histogram of call rate (proportion of SNPs scored) for each sample.
 """,
-        'KGD/Co-call-HWdgm.05.png' : """
+        'Co-call-HWdgm.05.png' : """
 Shows the co-call distribution (see Co-call-.png) after applying the Hardy-Weinberg filter to SNPs and combining lanes (if relevant).
 """,
-        'KGD/Co-call-.png' : """
+        'Co-call-.png' : """
 The co-call plot shows the distribution (over all possible pairs) of the proportion of SNPs called in a pair of individuals.
 Bimodality may indicate that the samples belong to 2+ genetically divergent clusters (low co-call rate for pairs from different clusters)
 """,
-        'KGD/finplot.png' : """
+        'finplot.png' : """
 A finplot shows raw Hardy-Weinberg disequilibrium (PAA - pA^2) against MAF for each SNP. 
 SNPs near the upper border tend to be mainly homozygous. This is often because they have low depth (perhaps only one allele observed).
 SNPs near the lower border tend to be mainly heterozygous and may often represent duplicated regions erroneously treated as one region. 
 The Hardy-Weinberg filter, HWdgm.05 (Hardy-Weinberg disequilibium > -0.05) used in some analyses removes these heterozygous SNPs.
 """,
-        'KGD/Gcompare.png' : """
+        'Gcompare.png' : """
 Comparisons of different methods for calculating off-diagonals of the GRM. G5=KGD method, G3=KGD with single allele per genotype, G1=unadjusted (biased towards 0).
 """,
-        'KGD/GcompareHWdgm.05.png' : """
+        'GcompareHWdgm.05.png' : """
 Same as Gcompare.png plot but using only those SNPs passing the Hardy-Weinberg filter and combining lanes (if relevant)
 """,
-        'KGD/SampDepthHist.png' : """
+        'SampDepthHist.png' : """
 Distribution of sample depths. If it is not roughly unimodal, there may be contamination or a mix of different species or breeds. Also, some downstream
  pipelines use a sample-depth cutoff to filter the data, which could be confused by a multi-modal sample-depth distribution.
 """,
-        'KGD/Gdiagdepth.png' : """
+        'Gdiagdepth.png' : """
 In theory self-relatedness estimated from GBS data should be uncorrelated with sample depth, so there should be neither negative nor positive trend
  in this plot. A negatively sloping trend has been nick-named a slippery slope, and means something is not quite right somewhere (with either the make-up of the original sample
  -e.g. some kind of contamination, or the enzyme digest, or size selection, or sequencing, or downstream processing). If they persist, slippery slopes need to be investigated and
  resolved
 """,
-        'KGD/GHWdgm.05diagdepth.png' : """
+        'GHWdgm.05diagdepth.png' : """
 This is similar to the Gdiagdepth.png plot, but 1) combining lanes (if relevant) and 2) based on a smaller set of SNPs (net of the Hardy-Weinberg filter), which eliminates some potential sources of
  correlation between self-relatedness estimates and sample depth. So this plot should exhibit less than (or the same) correlation as that plot. 
 """,
-        'KGD/Heatmap-G5HWdgm.05.png' : """
+        'Heatmap-G5HWdgm.05.png' : """
 Heatmap representation of the G5 GRM.
 Relatedness values are coloured from white to red (lowest to highest relatedness). Samples are ordered by a dendrogram from hierarchically clustering a distance matrix of the GRM.
 SNPs have been Hardy-Weinberg filterd and and lanes combined (if relevant)
 """,
-        'KGD/HWdisMAFsig.png' : """
+        'HWdisMAFsig.png' : """
 SNPs plotted as in finplot.png but coloured by an approximate depth-adjusted significance value.
 """,
-        'KGD/MAF.png' : """
+        'MAF.png' : """
 MAF distribution. Diverse populations tend to have a peak at low values of MAF.
 """,
-        'KGD/MAFHWdgm.05.png' : """
+        'MAFHWdgm.05.png' : """
 MAF distribution of Hardy-Weinberg filtered SNPs after combining lanes (if relevant)
 """,
-        'KGD/PC1v2G5HWdgm.05.png' : """
+        'PC1v2G5HWdgm.05.png' : """
 Principal components plot (1st 2 coordinates) of samples, based on the G5 GRM.
 SNPs have been Hardy-Weinberg filterd and and lanes combined (if relevant)
 """,
-        'KGD/SampDepthCR.png' : """
+        'SampDepthCR.png' : """
 Plot of depth vs call rate for samples. This is usually a fairly tight upwardly curving line.
 """,
-        'KGD/SampDepthHist.png' : """
+        'SampDepthHist.png' : """
 Histogram of sample depths. This is usually unimodal. 
 """,
-        'KGD/SampDepth.png' : """
+        'SampDepth.png' : """
 Plot of mean vs median sample depth.
 """,
-        'KGD/SampDepth-scored.png' : """
+        'SampDepth-scored.png' : """
 Plot of mean sample depth for SNPs with at least one read for the sample against the mean sample depth including all SNPs.
 """,
-        'KGD/SNPCallRate.png' : """
+        'SNPCallRate.png' : """
 Histogram of the proportion of samples called for a SNP. There is usually a peak at a high value and tails off to the left.
 """,
-        'KGD/SNPDepthHist.png' : """
+        'SNPDepthHist.png' : """
 Histogram of mean SNP depth. There are often a few SNPs with extremely high depth, while most SNPs have relatively low depth. 
 The SNPdepth.png plot has SNP depth log-transformed to show the distribution more clearly.
 """,
-        'KGD/SNPDepth.png' : """
+        'SNPDepth.png' : """
 Plot of SNP call rate against (log-transformed) SNP depth. Usually a S-shaped band. 
 High depth low call rate SNPs may indicate diverse populations (some SNPs only seen in a subset) (and/or possibly size selection variation?)
 """,
-        'KGD/X2star-QQ.png' : """
+        'X2star-QQ.png' : """
 Quantile-quantile plot of approximate depth-adjusted Hardy-Weinberg test statistic for each SNP. A 1-1 line indicates the theoretical distribution holds. 
 A higher sloped straight line suggests some population structure. SNPs that plot higher than a straight line through the majority of SNPs may indicate SNPs that do not behave in a Mendelian fashion.
 """,
-        'KGD/PlateDepth.png' : """
+        'PlateDepth.png' : """
 Plot of mean sample depth by plate position. Patterns of depth variation may represent problems with sample handling.
 """,
-        'KGD/PlateInb.png' : """
+        'PlateInb.png' : """
 Plot of estimated inbreeding by plate position. Patterns of inbreeding variation may represent problems with sample handling.
 """,
-        'KGD/SubplateDepth.png' : """
+        'SubplateDepth.png' : """
 The same as PlateDepth.png but with a different colour gradient for each subplate within the main plate.
 """,
-        'KGD/SubplateInb.png' : """
+        'SubplateInb.png' : """
 The same as PlateInb.png but with a different colour gradient for each subplate within the main plate.
 """
     } 
@@ -242,8 +247,8 @@ The same as PlateInb.png but with a different colour gradient for each subplate 
 
                             if os.path.isfile(file_path):
                                 title = image_relpath
-                                if file_name in narratives:
-                                    title = "\"%s (%s)\""%(image_relpath, narratives[file_name].replace('\n',''))
+                                if os.path.basename(file_name) in narratives:
+                                    title = "\"%s (%s)\""%(image_relpath, narratives[os.path.basename(file_name)].replace('\n',''))
                                 print >> out_stream, "<td> <img src=%s title=%s height=300 width=300/> </td>\n"%(image_relpath, title)
                                
                             else:
