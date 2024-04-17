95be46ee7118b38825c913fca3732f9126cddf6b
Simon Guest <simon.guest@agresearch.co.nz>
2024-04-17 14:49:35 +1200

    Initial flake with snakemake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for gbs_prism development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+          with pkgs;
+          {
+            devShells = {
+              default = mkShell {
+                buildInputs = [
+                  snakemake
+                ];
+              };
+            };
+          }
+      );
+}
