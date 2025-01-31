26966ac28992b616243a8a8afa22481cd28053dc
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-31 14:05:28 +1300

    Fix util exports

--- src/agr/util/__init__.py
+++ src/agr/util/__init__.py
@@ -1,6 +1,18 @@
 # re-exports for agr.util
 
+from . import iterator, legacy, path, redun, subprocess
 from .error import eprint
 from .singleton import singleton, Singleton
 
-__all__ = ["eprint", "singleton", "Singleton"]
+__all__ = [
+    # packages
+    "iterator",
+    "legacy",
+    "path",
+    "redun",
+    "subprocess",
+    # symbols
+    "eprint",
+    "singleton",
+    "Singleton",
+]
