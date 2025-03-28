aa4a66227668616b82d8db447230ed1445e328ee
Simon Guest <simon.guest@agresearch.co.nz>
2025-03-28 13:44:40 +1300

    Some rationalisation of make_clientcohort_pages

--- src/agr/gbs_prism/make_clientcohort_pages.py
+++ src/agr/gbs_prism/make_clientcohort_pages.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 
 
-#
-# this script creates a client oriented html document for each cohort of
-# a gbs run (or for a specified cohort foilder) , which presents the various plots generated , and
-# has links to text output. Its also calls utilities to make a pdf and
-# marshall the output to be sent.
-#
-import os
-import re
-import argparse
-
+import os.path
 
 header1 = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
    "httpd://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -33,36 +24,20 @@ footer1 = """
 """
 
 
-def get_cohorts(options):
-    BASEDIR = options["basedir"]
-    # cohorts are idenitified as subfolders of the run folder that
-    # * are not tardis working folders (i.e. have names starting with tardis
-    # * are of like SQ0775.all.TILAPIA.PstI-MspI
-    #   - i.e. library.qc-cohort.gbs-cohort.enzyme
-    run_folder = os.path.join(BASEDIR, options["run_folder"])
-    # print "DEBUG : "+run_folder
-    # SQ0810.all.PstI-MspI.PstI-MspI
-    # SQ0812.all.ApeKI.ApeKI
-    # SQ2768.all.ApeKI.ApeKI
-    # SQ2769.all.ApeKI.ApeKI
-    # SQ2770.all.ApeKI.ApeKI
-
-    cohort_folders = [
-        node
-        for node in os.listdir(run_folder)
-        if re.search("^tardis", node) is None
-        and re.search(r"^\S+\.\S+\.\S+\.\S+$", node) is not None
-        and os.path.isdir(os.path.join(run_folder, node))
-        and re.search("^OLD_", node) is None
-    ]
-
-    return cohort_folders
-
-
-def generate_run_plot(options):
-    BASEDIR = options[
-        "basedir"
-    ]  # e.g. /bifo/scratch/2024_illumina_sequencing_e/postprocessing/gbs
+def make_clientcohort_page(
+    base_dir: str,
+    cohort: str,
+    out_path: str,
+    title_prefix: str = "",  # allows for "filtered "
+    kgd_subfolder_name: str = "KGD",  # allows for filtered_KGD in case we ever do that
+    hapmap_subfolder_name: str = "hapMap",  # allows for filtered_KGD in case we ever do that
+):
+    """
+    create a client oriented html document for each cohort of
+    a gbs run (or for a specified cohort foilder) , which presents the various plots generated , and
+    has links to text output. Its also calls utilities to make a pdf and
+    marshall the output to be sent.
+    """
 
     file_group_iter = (
         ("KGD (plots)", "image"),
@@ -71,65 +46,62 @@ def generate_run_plot(options):
     )
     file_iters = {
         "KGD (plots)": [
-            "AlleleFreq.png",
-            "CallRate.png",
-            "Co-call-HWdgm.05.png",
-            "Co-call-.png",
-            "finplot.png",
-            "GcompareHWdgm.05.png",
-            "Gcompare.png",
-            "Gdiagdepth.png",
-            "GHWdgm.05diagdepth.png",
-            "InbCompare.png",
-            "Heatmap-G5HWdgm.05.png",
-            "HWdisMAFsig.png",
-            "MAFHWdgm.05.png",
-            "MAF.png",
-            "PC1v2G5HWdgm.05.png",
-            "SampDepthCR.png",
-            "SampDepthHist.png",
-            "SampDepth.png",
-            "SampDepth-scored.png",
-            "SNPCallRate.png",
-            "SNPDepthHist.png",
-            "SNPDepth.png",
-            "X2star-QQ.png",
-            "PlateDepth.png",
-            "PlateInb.png",
-            "SubplateDepth.png",
-            "SubplateInb.png",
+            os.path.join(kgd_subfolder_name, item)
+            for item in [
+                "AlleleFreq.png",
+                "CallRate.png",
+                "Co-call-HWdgm.05.png",
+                "Co-call-.png",
+                "finplot.png",
+                "GcompareHWdgm.05.png",
+                "Gcompare.png",
+                "Gdiagdepth.png",
+                "GHWdgm.05diagdepth.png",
+                "InbCompare.png",
+                "Heatmap-G5HWdgm.05.png",
+                "HWdisMAFsig.png",
+                "MAFHWdgm.05.png",
+                "MAF.png",
+                "PC1v2G5HWdgm.05.png",
+                "SampDepthCR.png",
+                "SampDepthHist.png",
+                "SampDepth.png",
+                "SampDepth-scored.png",
+                "SNPCallRate.png",
+                "SNPDepthHist.png",
+                "SNPDepth.png",
+                "X2star-QQ.png",
+                "PlateDepth.png",
+                "PlateInb.png",
+                "SubplateDepth.png",
+                "SubplateInb.png",
+            ]
         ],
         "KGD (text file links)": [
-            "GHW05.csv",
-            "GHW05-Inbreeding.csv",
-            "GHW05-long.csv",
-            "GHW05-pca_metadata.tsv",
-            "GHW05-pca_vectors.tsv",
-            "GHW05-PC.csv",
-            "GHW05.RData",
-            "GHW05.vcf",
-            "HeatmapOrderHWdgm.05.csv",
-            "HeatmapOrderHWdgm.05.csv.blinded",
-            "PCG5HWdgm.05.pdf",
-            "SampleStats.csv",
-            "SampleStats.csv.blinded",
-            "seqID.csv",
-            "seqID.csv.blinded",
+            os.path.join(kgd_subfolder_name, item)
+            for item in [
+                "GHW05.csv",
+                "GHW05-Inbreeding.csv",
+                "GHW05-long.csv",
+                "GHW05-pca_metadata.tsv",
+                "GHW05-pca_vectors.tsv",
+                "GHW05-PC.csv",
+                "GHW05.RData",
+                "GHW05.vcf",
+                "HeatmapOrderHWdgm.05.csv",
+                "HeatmapOrderHWdgm.05.csv.blinded",
+                "PCG5HWdgm.05.pdf",
+                "SampleStats.csv",
+                "SampleStats.csv.blinded",
+                "seqID.csv",
+                "seqID.csv.blinded",
+            ]
         ],
-        "Hapmap files": ["HapMap.hmc.txt", "HapMap.fas.txt"],
+        "Hapmap files": [
+            os.path.join(hapmap_subfolder_name, item)
+            for item in ["HapMap.hmc.txt", "HapMap.fas.txt"]
+        ],
     }
-    file_iters["KGD (plots)"] = [
-        os.path.join(options["kgd_subfolder_name"], item)
-        for item in file_iters["KGD (plots)"]
-    ]
-    file_iters["KGD (text file links)"] = [
-        os.path.join(options["kgd_subfolder_name"], item)
-        for item in file_iters["KGD (text file links)"]
-    ]
-    file_iters["Hapmap files"] = [
-        os.path.join(options["hapmap_subfolder_name"], item)
-        for item in file_iters["Hapmap files"]
-    ]
 
     narratives = {
         "InbCompare.png": """
@@ -241,208 +213,63 @@ SNPs tracking an intermediate slope (observed) may be due to non-diploid genome.
 """,
     }
 
-    # print "DEBUG : calling get_cohorts"
-    if options["cohort_folder"] is not None:
-        cohorts = [options["cohort_folder"]]
-        out_stream_filename = options["output_filename"]
-        out_manifest_filename = "%s.manifest" % options["output_filename"]
-    else:
-        cohorts = get_cohorts(options)
+    manifest_path = "%s.manifest" % out_path
+    base_relpath = os.path.relpath(base_dir, out_path)
 
-    for cohort in cohorts:
+    with open(out_path, "w") as out_f:
 
-        if options["cohort_folder"] is None:
-            out_stream_filename = os.path.join(
-                BASEDIR,
-                options["run_folder"],
-                "html",
-                cohort,
-                options["output_filename"],
-            )
-            out_manifest_filename = os.path.join(
-                BASEDIR,
-                options["run_folder"],
-                "html",
-                cohort,
-                "%s.manifest" % options["output_filename"],
-            )
+        with open(manifest_path, "w") as manifest_f:
 
-        # print cohort, out_stream_filename, out_manifest_filename
+            _ = manifest_f.write("%s\n" % out_path)
+            _ = manifest_f.write("%s" % manifest_path)
 
-        with open(out_stream_filename, "w") as out_stream:
+            _ = out_f.write(header1 % {"title": "%sKGD" % title_prefix})
 
-            with open(out_manifest_filename, "w") as out_manifest:
-
-                _ = out_manifest.write("%s\n" % options["output_filename"])
-                _ = out_manifest.write("%s.manifest" % options["output_filename"])
-
-                if options["title"] is None:
-                    _ = out_stream.write(header1 % {"title": cohort})
-                else:
-                    _ = out_stream.write(header1 % options)
-
-                for file_group, file_type in file_group_iter:
-                    # output overall header for file group
-                    _ = out_stream.write("<h2> %s </h2>\n" % file_group)
-                    # output cohort column headings
-                    _ = out_stream.write(
-                        f"""<table width=90% align=center>
+            for file_group, file_type in file_group_iter:
+                # output overall header for file group
+                _ = out_f.write("<h2> %s </h2>\n" % file_group)
+                # output cohort column headings
+                _ = out_f.write(
+                    f"""<table width=90% align=center>
 <tr>
 <td> <h4> Name </h4> </td>
 {"\n".join(["<td><h4> %s </h4></td>" % cohort for cohort in [cohort]])}
 </tr>
 """
-                    )
-                    for file_name in file_iters[file_group]:
+                )
+                for file_name in file_iters[file_group]:
 
-                        _ = out_stream.write("<tr><td>%s</td>\n" % file_name)
-                        if options["run_folder"] is not None:
-                            file_path = os.path.join(
-                                BASEDIR, options["run_folder"], cohort, file_name
+                    _ = out_f.write("<tr><td>%s</td>\n" % file_name)
+                    file_path = os.path.join(base_dir, file_name)
+                    file_relpath = os.path.join(base_relpath, file_name)
+                    _ = manifest_f.write("%s\n" % file_relpath)
+                    if file_type == "image":
+                        if os.path.isfile(file_path):
+                            if os.path.basename(file_name) in narratives:
+                                image_title = '"%s (%s)"' % (
+                                    file_relpath,
+                                    narratives[os.path.basename(file_name)].replace(
+                                        "\n", ""
+                                    ),
+                                )
+                            else:
+                                image_title = file_relpath
+                            _ = out_f.write(
+                                "<td> <img src=%s title=%s height=300 width=300/> </td>\n\n"
+                                % (file_relpath, image_title)
+                            )
+
+                        else:
+                            _ = out_f.write("<td> unavailable </td>\n\n")
+                    elif file_type == "link":
+
+                        if os.path.isfile(file_path):
+                            _ = out_f.write(
+                                "<td width=300> <a href=%s target=%s> %s </a></td>\n\n"
+                                % (file_relpath, file_name, file_relpath)
                             )
                         else:
-                            file_path = os.path.join(cohort, file_name)
-
-                        if file_type == "image":
-                            image_relpath = file_name
-
-                            _ = out_manifest.write("%s\n" % image_relpath)
-
-                            if os.path.isfile(file_path):
-                                title = image_relpath
-                                if os.path.basename(file_name) in narratives:
-                                    title = '"%s (%s)"' % (
-                                        image_relpath,
-                                        narratives[os.path.basename(file_name)].replace(
-                                            "\n", ""
-                                        ),
-                                    )
-                                _ = out_stream.write(
-                                    "<td> <img src=%s title=%s height=300 width=300/> </td>\n\n"
-                                    % (image_relpath, title)
-                                )
-
-                            else:
-                                _ = out_stream.write("<td> unavailable </td>\n\n")
-                        elif file_type == "link":
-                            link_relpath = file_name
-
-                            _ = out_manifest.write("%s\n" % link_relpath)
-
-                            if os.path.isfile(file_path):
-                                _ = out_stream.write(
-                                    "<td width=300> <a href=%s target=%s> %s </a></td>\n\n"
-                                    % (link_relpath, file_name, link_relpath)
-                                )
-                            else:
-                                _ = out_stream.write(
-                                    "<td width=300> unavailable </td>\n\n"
-                                )
-                        _ = out_stream.write("</tr>\n\n")
-                    _ = out_stream.write("</table>\n\n")
-                _ = out_stream.write("%s\n" % footer1)
-
-
-def get_options():
-    description = """
-    """
-    long_description = """
-examples :
-
-# for a standard hiseq run 
-
-./make_clientcohort_pages.py -r 181112_D00390_0415_BCD04NANXX -o report.html
-key file summary looks like :
-
-run     run_number      lane    samplename      species file_name
-140624_D00390_0044_BH9PEBADXX   0044    1       SQ0001  Deer    140624_D00390_0044_BH9PEBADXX.gbs/SQ0001.processed_sample/uneak/kmer_analysis/kmer_zipfian_comparisons.jpg
-140624_D00390_0044_BH9PEBADXX   0044    2       SQ0001  Deer    140624_D00390_0044_BH9PEBADXX.gbs/SQ0001.processed_sample/uneak/kmer_analysis/kmer_zipfian_comparisons.jpg
-140904_D00390_0209_BC4U6YACXX   0209    1       SQ0008  Deer    140904_D00390_0209_BC4U6YACXX.gbs/SQ0008.processed_sample/uneak/kmer_analysis/kmer_zipfian_comparisons.jpg
-140904_D00390_0209_BC4U6YACXX   0209
-
-
-# for some custom runs
-python ../gbs_prism/make_clientcohort_pages.py -t "Unfiltered run" -o /dataset/gseq_processing/itmp/Gqueries/uneak_kgd/gbsathon_adapter/PstI-MspI/unfiltered.html /dataset/gseq_processing/itmp/Gqueries/uneak_kgd/gbsathon_adapter/PstI-MspI
-python ../gbs_prism/make_clientcohort_pages.py -U filtered_hapMap -K filtered_KGD -t "Filtered run" -o /dataset/gseq_processing/itmp/Gqueries/uneak_kgd/gbsathon_adapter/PstI-MspI/filtered.html /dataset/gseq_processing/itmp/Gqueries/uneak_kgd/gbsathon_adapter/PstI-MspI
-python ../gbs_prism/make_clientcohort_pages.py -U discarded_hapMap -K discarded_KGD -t "Filtered run - using discarded tags" -o /dataset/gseq_processing/itmp/Gqueries/uneak_kgd/gbsathon_adapter/PstI-MspI/discarded.html /dataset/gseq_processing/itmp/Gqueries/uneak_kgd/gbsathon_adapter/PstI-MspI
-
-
-    """
-    parser = argparse.ArgumentParser(
-        description=description,
-        epilog=long_description,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-    _ = parser.add_argument("cohort_folder", nargs="?", type=str, default=None)
-    _ = parser.add_argument(
-        "-r", "--run_folder", dest="run_folder", default=None, type=str, help="run name"
-    )
-    _ = parser.add_argument(
-        "-t", "--title", dest="title", default=None, type=str, help="title"
-    )
-    _ = parser.add_argument(
-        "-H",
-        "--image_height",
-        dest="image_height",
-        default=300,
-        type=int,
-        help="image height",
-    )
-    _ = parser.add_argument(
-        "-W",
-        "--image_width",
-        dest="image_width",
-        default=300,
-        type=int,
-        help="image width",
-    )
-    _ = parser.add_argument(
-        "-o",
-        "--output_filename",
-        dest="output_filename",
-        default="peacock.html",
-        type=str,
-        help="name of output file",
-    )
-    _ = parser.add_argument(
-        "-U",
-        "--hapmap_subfolder_name",
-        dest="hapmap_subfolder_name",
-        default="hapMap",
-        type=str,
-        help="name of hapmap subfolder",
-    )
-    _ = parser.add_argument(
-        "-K",
-        "--kgd_subfolder_name",
-        dest="kgd_subfolder_name",
-        default="KGD",
-        type=str,
-        help="name of KGD sub-folder name",
-    )
-    _ = parser.add_argument(
-        "-b",
-        "--basedir",
-        dest="basedir",
-        default="/dataset/2024_illumina_sequencing_e/scratch/postprocessing/gbs",
-        type=str,
-        help="base dir of original output",
-    )
-
-    args = vars(parser.parse_args())
-
-    if args["run_folder"] is None and args["cohort_folder"] is None:
-        raise Exception("must specify either run folder or a cohort folder")
-
-    return args
-
-
-def main():
-
-    options = get_options()
-    # print options
-    generate_run_plot(options)
-
-
-if __name__ == "__main__":
-    main()
+                            _ = out_f.write("<td width=300> unavailable </td>\n\n")
+                    _ = out_f.write("</tr>\n\n")
+                _ = out_f.write("</table>\n\n")
+            _ = out_f.write("%s\n" % footer1)
