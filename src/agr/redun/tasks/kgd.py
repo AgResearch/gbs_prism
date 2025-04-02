7a987e4f72c261920fcad84f79a1bc95b321ecb4
Benjamin J Perry <ben.perry@agresearch.co.nz>
2025-04-02 13:13:31 +1300

    Extending KgdOutput class and adding unblind tasks (#89)
    updating the naming convention for the sed script
    
    updating sytnax for consistency
    
    extending the KGD class to track all needed files
    
    attempting to collect KGD output Files as a list of Files for unblind_all
    
    removing list comprehension from unblind_all parameters
    
    attempting to track KGDstdout
    
    reverting the KGD stdout and stderr log paths
    
    debuggin list comprehension for kgd_unblind
    
    Fix KGD blinded files list using lazy_map
    
    Redun lazy expressions means that inline list comprehensions don't
    work.  See e.g. other uses of lazy_map.

--- src/agr/redun/tasks/kgd.py
+++ src/agr/redun/tasks/kgd.py
@@ -10,6 +10,20 @@ logger = logging.getLogger(__name__)
 # keys for files, not filenames
 KGD_SAMPLE_STATS = "sample-stats"
 KGD_GUSBASE_RDATA = "gusbase-rdata"
+GHW05_CSV = "ghw05-csv"
+GHW05_VCF = "ghw05-vcf"
+GHW05_INBREEDING_CSV = "ghw05-inbreeding-csv"
+GHW05_LONG_CSV = "ghw05-long-csv"
+GHW05_PC_CSV = "ghw05-pc-csv"
+GHW05_PCA_METADATA_TSV = "ghw05-pca-metadata-tsv"
+GHW05_PCA_VECTORS_TSV = "ghw05-pca-vectors-tsv"
+HEATMAP_ORDER_HWDGM05_CSV = "heatmap-order-hwdgm05-csv"
+HIGH_RELATEDNESS_CSV = "high-relatedness-csv"
+HIGH_RELATENESS_SPLIT_CSV = "high-relateness-split-csv"
+SEQ_ID_CSV = "seq-id-csv"
+SAMPLE_STATS_RAW_CSV = "sample-stats-raw-csv"
+SAMPLE_STATS_RAW_COMBINED_CSV = "sample-stats-raw-combined-csv"
+KGD_STDOUT = "kgd-stdout"
 
 KGD_TOOL_NAME = "KGD"
 
@@ -44,6 +58,26 @@ def _kgd_job_spec(
         expected_paths={
             KGD_SAMPLE_STATS: os.path.join(out_dir, "SampleStats.csv"),
             KGD_GUSBASE_RDATA: os.path.join(out_dir, "GUSbase.RData"),
+            GHW05_CSV: os.path.join(out_dir, "GHW05.csv"),
+            GHW05_VCF: os.path.join(out_dir, "GHW05.vcf"),
+            GHW05_INBREEDING_CSV: os.path.join(out_dir, "GHW05-Inbreeding.csv"),
+            GHW05_LONG_CSV: os.path.join(out_dir, "GHW05-long.csv"),
+            GHW05_PC_CSV: os.path.join(out_dir, "GHW05-PC.csv"),
+            GHW05_PCA_METADATA_TSV: os.path.join(out_dir, "GHW05-pca_metadata.tsv"),
+            GHW05_PCA_VECTORS_TSV: os.path.join(out_dir, "GHW05-pca_vectors.tsv"),
+            HEATMAP_ORDER_HWDGM05_CSV: os.path.join(
+                out_dir, "HeatmapOrderHWdgm.05.csv"
+            ),
+            HIGH_RELATEDNESS_CSV: os.path.join(out_dir, "HighRelatedness.csv"),
+            HIGH_RELATENESS_SPLIT_CSV: os.path.join(
+                out_dir, "HighRelatedness.split.csv"
+            ),
+            SEQ_ID_CSV: os.path.join(out_dir, "seqID.csv"),
+            SAMPLE_STATS_RAW_CSV: os.path.join(out_dir, "SampleStatsRaw.csv"),
+            SAMPLE_STATS_RAW_COMBINED_CSV: os.path.join(
+                out_dir, "SampleStatsRawCombined.csv"
+            ),
+            KGD_STDOUT: out_path,
         },
         expected_globs={},
     )
@@ -53,6 +87,25 @@ def _kgd_job_spec(
 class KgdOutput:
     sample_stats_csv: File
     gusbase_rdata: File
+    ghw05_csv: File
+    ghw05_vcf: File
+    ghw05_inbreeding_csv: File
+    ghw05_long_csv: File
+    ghw05_PC_csv: File
+    ghw05_pca_metadata_tsv: File
+    ghw05_pca_vectors_tsv: File
+    heatmap_order_hwdgm05_csv: File
+    high_relatedness_csv: File
+    high_relateness_split_csv: File
+    seq_ID_csv: File
+    sample_stats_raw_csv: File
+    sample_stats_raw_combined_csv: File
+    kgd_stdout: File
+
+
+def kgd_output_files(kgd_output: KgdOutput) -> list[File]:
+    """Return all output as a list of files."""
+    return [file for file in vars(kgd_output).values()]
 
 
 @task()
@@ -84,4 +137,24 @@ def kgd(work_dir: str, genotyping_method: str, hap_map_files: list[File]) -> Kgd
     return KgdOutput(
         sample_stats_csv=result_files.expected_files[KGD_SAMPLE_STATS],
         gusbase_rdata=result_files.expected_files[KGD_GUSBASE_RDATA],
+        ghw05_csv=result_files.expected_files[GHW05_CSV],
+        ghw05_vcf=result_files.expected_files[GHW05_VCF],
+        ghw05_inbreeding_csv=result_files.expected_files[GHW05_INBREEDING_CSV],
+        ghw05_long_csv=result_files.expected_files[GHW05_LONG_CSV],
+        ghw05_PC_csv=result_files.expected_files[GHW05_PC_CSV],
+        ghw05_pca_metadata_tsv=result_files.expected_files[GHW05_PCA_METADATA_TSV],
+        ghw05_pca_vectors_tsv=result_files.expected_files[GHW05_PCA_VECTORS_TSV],
+        heatmap_order_hwdgm05_csv=result_files.expected_files[
+            HEATMAP_ORDER_HWDGM05_CSV
+        ],
+        high_relatedness_csv=result_files.expected_files[HIGH_RELATEDNESS_CSV],
+        high_relateness_split_csv=result_files.expected_files[
+            HIGH_RELATENESS_SPLIT_CSV
+        ],
+        seq_ID_csv=result_files.expected_files[SEQ_ID_CSV],
+        sample_stats_raw_csv=result_files.expected_files[SAMPLE_STATS_RAW_CSV],
+        sample_stats_raw_combined_csv=result_files.expected_files[
+            SAMPLE_STATS_RAW_COMBINED_CSV
+        ],
+        kgd_stdout=result_files.expected_files[KGD_STDOUT],
     )
