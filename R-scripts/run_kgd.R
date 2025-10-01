64be55ec8b83595318914c924ab70e8653ed1e82
Rudi Brauning <rudiger.brauning@agresearch.co.nz>
2025-10-01 13:41:46 +1300

    Merging Improve VCF (#186) into next release build branch
    * Add demultiplex parameter merge_taxa for Tassel merge_taxa_tag_count (#177)
    
    Previously this had been incorrectly coupled with whether there was
    one or multiple parts returned from ramify_tassel_keyfile.
    
    Co-authored-by: Simon Guest <simon.guest@agresearch.co.nz>
    
    * Improve VCF
    
    Use additional writeVCF parameters. o	A minimum depth of 1 is required to obtain a genotype. The absolute genotype (GT) matches the highest ranked probabilistic genotype (GP).
    
    ---------
    
    Co-authored-by: Simon Guest <simon.guest@tesujimath.org>
    Co-authored-by: Simon Guest <simon.guest@agresearch.co.nz>

--- R-scripts/run_kgd.R
+++ R-scripts/run_kgd.R
@@ -163,7 +163,7 @@ if ( geno_method == "default" ) {
     GHWdgm.05 <- calcG(snpsubset=which(HWdis.sep > -0.05),sfx="HWdgm.05",npc=4,puse=p.sep)
   }
   writeG(GHWdgm.05, "GHW05", outtype=c(1, 2, 3, 4, 5, 6))
-  writeVCF(outname="GHW05", ep=.001)
+  writeVCF(outname="GFULL", ep=.001, mindepth=1, GTmethod="GP")
   keypos <- match(seq2samp(seqID,nparts=npartsID),seqinfo$sample)
   if((DO_KGD_PLATE_PLOTS == "yes") && any(!is.na(samppos)))  {
      if(length(table(seqinfo$platename[keypos]))==1) {
@@ -175,7 +175,7 @@ if ( geno_method == "default" ) {
 } else if ( geno_method == "pooled" ) {
   Gfull <- calcG(samptype=geno_method, npc=4)
   writeG(Gfull, "GFULL", outtype=c(1, 2, 3, 4, 5, 6))
-  writeVCF(outname="GFULL", ep=.001)
+  writeVCF(outname="GFULL", ep=.001, mindepth=1, GTmethod="GP")
   keypos <- match(seq2samp(seqID,nparts=npartsID),seqinfo$sample)
   if((DO_KGD_PLATE_PLOTS == "yes") && any(!is.na(samppos)))  {
      if(length(table(seqinfo$platename[keypos]))==1) {
